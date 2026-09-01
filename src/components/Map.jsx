c0b4081db1a691512fdd1dc74bab426870b2f27c
Menoor <noorpazhouhm@gmail.com>
2026-09-01 16:29:43 +0330

    added the map with leaflet library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,21 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useCities } from "../contexts/CitiesContext";
+
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const { cities } = useCities();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
@@ -15,11 +26,38 @@ function Map() {
         navigate("form");
       }}
     >
-      <h1>Map</h1>
-      <h1>
-        Position : {lat} , {lng}{" "}
-      </h1>
+      <MapContainer
+        className={styles.map}
+        center={[52.53586782505711, 13.376933665713324]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <DetectClick />
+      </MapContainer>
     </div>
   );
 }
+function DetectClick() {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      console.log(lat, lng);
+    },
+  });
+}
+
 export default Map;
